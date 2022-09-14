f17f8980f9285df3142cc069ef15fa54c7b7508e
thanhcong227 <106806176+thanhcong227@users.noreply.github.com>
2022-09-14 08:45:21 +0700

    second commit

---
+++ untitled/src/hw1_javareview/PerfectNumber.java
@@ -0,0 +1,31 @@
+package hw1_javareview;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            if (perfectNumber(arr[i])) {
+                System.out.println("Perfect number :"+arr[i]);
+            } else {
+                System.out.println("NOT Perfect number :"+arr[i]);
+            }
+        }
+    }
+
+    public static boolean perfectNumber(int m) {
+        int sum = 0;
+        for (int i = 1; i <= m / 2; i++) {
+            if (m % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == m;
+    }
+}
